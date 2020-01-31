e4c642fbbe48838c96e0c115b22d6404cb1a20cb
Dragan <petar.novakovic11@gmail.com>
2020-01-31 17:58:55 +0100

    Intersection homework refactor

--- src/NemanjaLista/NemanjaDomaciLIste.java
+++ src/NemanjaLista/NemanjaDomaciLIste.java
@@ -5,35 +5,47 @@ import java.util.List;
 
 public class NemanjaDomaciLIste {
 	public static void main(String[] args) {
+		List<Integer> firstList = createFirstList();		
+		List<Integer> secondList = createSecondList();		
+		List<Integer> intersection = findIntersection(firstList, secondList);
+		printList(intersection);
+	}
+	
+	private static void printList(List<Integer> intersection) {
+		for (Integer integer : intersection) {
+			System.out.println(integer);
+		};
+	}
+
+	private static List<Integer> findIntersection(List<Integer> firstList,
+			List<Integer> secondList) {
+		List<Integer> intersection = new ArrayList<>();
+		for (int i = 0; i < firstList.size(); i++) {
+			for (int j = 0; j < secondList.size(); j++) {
+				if (firstList.get(i).equals(secondList.get(j))) {
+					intersection.add(firstList.get(i));
+				}
+			}
+		}
+		return intersection;
+	}
+
+	private static List<Integer> createSecondList() {
+		List<Integer> lista2 = new ArrayList<>();
+		lista2.add(22222);
+		lista2.add(333);
+		lista2.add(8);
+		return lista2;
+	}
+
+	private static List<Integer> createFirstList() {
 		List<Integer> lista1 = new ArrayList<>();
 		lista1.add(1);
-		lista1.add(2);
-		lista1.add(3);
+		lista1.add(22222);
+		lista1.add(333);
 		lista1.add(5);
 		lista1.add(6);
-		
-		List<Integer> lista2 = new ArrayList<>();
-		lista2.add(2);
-		lista2.add(3);
-		lista2.add(8);
-		
-		System.out.println("Elementi prve liste su: " + lista1);
-		System.out.println("Elementi druge liste su: " + lista2);
-		
-		List<Integer> presek = new ArrayList<>();
-		System.out.println("Zajednicki elementi su: " );
-		for (int i = 0; i < lista1.size(); i++) {
-			  for (int j = 0; j < lista2.size(); j++) {
-		
-				  if(lista1.get(i) == lista2.get(j)){
-                      presek.add(i);
-                      System.out.println(lista1.get(i));  
-                  }
-			  }	
-	}
-		
-	
-
+		return lista1;
 	}
 
 }
